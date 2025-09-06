0b2913d02e8322ba0ed899756450a130f30241fe
frostra1n <215321287+frostra1n@users.noreply.github.com>
2025-09-05 19:14:36 -0600

    Initial Commit

---
+++ Tanteador/TanteadorApp.swift
@@ -0,0 +1,17 @@
+//
+//  TanteadorApp.swift
+//  Tanteador
+//
+//  Created by Marco Carmona on 9/5/25.
+//
+
+import SwiftUI
+
+@main
+struct TanteadorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
