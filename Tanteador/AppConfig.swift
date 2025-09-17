74b13cf342a7f6030f006bab5a1762a704d9d08e
frostra1n <215321287+frostra1n@users.noreply.github.com>
2025-09-17 16:56:38 -0600

    fix network manager

---
+++ Tanteador/AppConfig.swift
@@ -0,0 +1,15 @@
+//
+//  AppConfig.swift
+//  Tanteador
+//
+//  Configuration management for the Tanteador app
+//
+
+import Foundation
+
+enum AppConfig {
+    enum API {
+        static let exchangeRateURL = "https://www.sucursalelectronica.com/ebac/common/GetExchangeRateInfo.go"
+        static let ipLocationURL = "https://ipapi.co/json/"
+    }
+}
--- Tanteador/NetworkManager.swift
+++ Tanteador/NetworkManager.swift
@@ -30,53 +30,38 @@ class NetworkManager {
     private init() {}
     
     func fetchExchangeRate() async throws -> ExchangeRateResponse {
-        let url = URL(string: "https://www.sucursalelectronica.com/ebac/common/GetExchangeRateInfo.go")!
-        
+        guard let url = URL(string: AppConfig.API.exchangeRateURL) else {
+            throw URLError(.badURL)
+        }
+
         var request = URLRequest(url: url)
         request.httpMethod = "POST"
-        
-        // Set headers
-        request.setValue("application/x-www-form-urlencoded;charset=UTF-8", forHTTPHeaderField: "Content-Type")
-        request.setValue("empty", forHTTPHeaderField: "Sec-Fetch-Dest")
-        request.setValue("*/*", forHTTPHeaderField: "Accept")
-        request.setValue("same-origin", forHTTPHeaderField: "Sec-Fetch-Site")
-        request.setValue("en-US,en;q=0.9", forHTTPHeaderField: "Accept-Language")
-        request.setValue("gzip, deflate, br", forHTTPHeaderField: "Accept-Encoding")
-        request.setValue("cors", forHTTPHeaderField: "Sec-Fetch-Mode")
-        request.setValue("https://www.sucursalelectronica.com", forHTTPHeaderField: "Origin")
-        request.setValue("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/18.6 Safari/605.1.15", forHTTPHeaderField: "User-Agent")
-        request.setValue("https://www.sucursalelectronica.com/redir/showLogin.go", forHTTPHeaderField: "Referer")
-        request.setValue("0", forHTTPHeaderField: "Content-Length")
-        request.setValue("keep-alive", forHTTPHeaderField: "Connection")
-        request.setValue("u=3, i", forHTTPHeaderField: "Priority")
-        
-        // Empty body for POST request
         request.httpBody = Data()
-        
+
         let (data, response) = try await URLSession.shared.data(for: request)
-        
-        guard let httpResponse = response as? HTTPURLResponse,
-              httpResponse.statusCode == 200 else {
-            throw URLError(.badServerResponse)
-        }
-        
-        let decoder = JSONDecoder()
-        decoder.keyDecodingStrategy = .convertFromSnakeCase
-        return try decoder.decode(ExchangeRateResponse.self, from: data)
+        return try parseResponse(data: data, response: response)
     }
-    
+
     func fetchUserLocation() async throws -> IPLocation {
-        let url = URL(string: "https://ipapi.co/json/")!
-        
+        guard let url = URL(string: AppConfig.API.ipLocationURL) else {
+            throw URLError(.badURL)
+        }
+
         let (data, response) = try await URLSession.shared.data(from: url)
-        
-        guard let httpResponse = response as? HTTPURLResponse,
-              httpResponse.statusCode == 200 else {
+        return try parseResponse(data: data, response: response)
+    }
+
+    private func parseResponse<T: Decodable>(data: Data, response: URLResponse) throws -> T {
+        guard let httpResponse = response as? HTTPURLResponse else {
             throw URLError(.badServerResponse)
         }
-        
+
+        guard httpResponse.statusCode == 200 else {
+            throw URLError(.badServerResponse)
+        }
+
         let decoder = JSONDecoder()
         decoder.keyDecodingStrategy = .convertFromSnakeCase
-        return try decoder.decode(IPLocation.self, from: data)
+        return try decoder.decode(T.self, from: data)
     }
 }
